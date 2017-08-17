7a06e3520dbcc37c9a052e215b60f3642ae5dd82
tab <tabchanj@gmail.com>
2017-08-17 16:54:25 +0800

    update gitignore

--- aladdin-manager/src/main/java/org/tabchanj/aladdin/controller/impl/LoginController.java
+++ aladdin-manager/src/main/java/org/tabchanj/aladdin/controller/impl/LoginController.java
@@ -1,10 +1,10 @@
 package org.tabchanj.aladdin.controller.impl;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/login/")
